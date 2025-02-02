c9e39da2c1e0412838d8a1bc3e6ad6fc8f16eaed
BNK48GreenFlash <kalosrsxy@gmail.com>
2025-02-03 00:58:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("tweetButton").addEventListener("click", tweetMessage);
+});
+
 function tweetMessage() {
     const message = "Arlee GreenFlash #GreenFlashConceptDay1 #BNK48 #BNK4818thSINGLE";
     const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`;
